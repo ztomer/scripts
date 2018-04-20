3ef224c23519efebd2d75b08b1f0132bba0ef5ba
Tomer Zaidentein <ztomer@gmail.com>
2018-04-20 08:39:44 +0300

    Update pip3_upgrade.py
    Adjust for pip v10

--- pip3_upgrade.py
+++ pip3_upgrade.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import pip
 from subprocess import call
+from pip._internal.utils.misc import get_installed_distributions
 
-for dist in pip.get_installed_distributions():
-	call("pip3 install --allow-external pycairo --allow-unverified pycairo --upgrade " + dist.project_name, shell=True)
+for dist in get_installed_distributions():
+	call("pip3 install  --upgrade " + dist.project_name, shell=True)
